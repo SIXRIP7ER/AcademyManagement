d27fb20e0f6d99afc26429db717508cbcd376424
JawadBakhtiari <126224960+JawadBakhtiari@users.noreply.github.com>
2023-11-29 14:02:48 +1100

    added routes to flask app, combined with create player

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, request
+from createPlayer import get_players, create_player
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return "Jawad is the best"
+    return "This is the main website"
+
+
+@app.route("/playerslist", methods=['GET'])
+def handle_players_list():
+    playersList = get_players()
+    # for p in playersList:
+    #     print(p)
+    return playersList
+
+
+@app.route("/createplayer", methods=['POST'])
+def handle_create_player():
+    request_data = request.get_json()
+
+    player_info = [
+        request_data['first_name'],
+        request_data['last_name'],
+        request_data['dob']
+    ]
+
+    create_player(player_info)
+
+    return "created player"
 
 
 # if __name__ == "__main__":
--- createPlayer.py
+++ createPlayer.py
@@ -2,50 +2,42 @@ import sys
 import psycopg2
 
 
-def getPlayers(db):
-    players = None
-    cur = db.cursor()
-    query = "SELECT * FROM Player"
-    cur.execute(query)
-    players = cur.fetchall()
-    cur.close()
-    return players
+def get_players():
+    db = None
+    try:
+        # connect to database
+        db = psycopg2.connect(
+            dbname="academy", user="postgres", host='192.168.0.137', port='2022', password='postgres')
+        players = None
+        cur = db.cursor()
+        query = "SELECT * FROM Player"
+        cur.execute(query)
+        players = cur.fetchall()
+        cur.close()
+        return players
+    except Exception as err:
+        print(err)
+    finally:
+        if db:
+            db.close()
 
 
-def createPlayer(db, p):
-    cur = db.cursor()
-    query = """
-            INSERT INTO Player VALUES
-            (%s,%s,%s)
-            """
-    cur.execute(query, [p[0], p[1], p[2]])
-    cur.close()
-    db.commit()
-    return None
-
-
-db = None
-try:
-    # connect to database
-    db = psycopg2.connect(
-        dbname="academy", user="postgres", host='192.168.0.137', port='2022', password='postgres')
-
-    # Create a player
-    pInfo = ["masi", "hashimi", "2002-02-16"]
-    createPlayer(db, pInfo)
-
-    # Create a player
-    pInfo = ["wali", "bakhtiari", "2002-02-16"]
-    createPlayer(db, pInfo)
-
-    # Retrieve Players
-    players = getPlayers(db)
-    for p in players:
-        print(p)
-
-
-except Exception as err:
-    print(err)
-finally:
-    if db:
-        db.close()
+def create_player(p):
+    db = None
+    try:
+        # connect to database
+        db = psycopg2.connect(
+            dbname="academy", user="postgres", host='192.168.0.137', port='2022', password='postgres')
+        cur = db.cursor()
+        query = """
+                INSERT INTO Player VALUES
+                (%s,%s,%s)
+                """
+        cur.execute(query, [p[0], p[1], p[2]])
+        cur.close()
+        db.commit()
+    except Exception as err:
+        print(err)
+    finally:
+        if db:
+            db.close()
