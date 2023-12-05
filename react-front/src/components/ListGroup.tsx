6e5806a02ccb9e5958fef31040a0c1f221712d0a
JawadBakhtiari <sixrip7er@gmail.com>
2023-12-05 18:08:07 +1100

    useState method which works

--- react-front/src/components/ListGroup.tsx
+++ react-front/src/components/ListGroup.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "Paris"];
   //   items = [];
-  let selectedIndex = -1;
+  //   let selectedIndex = -1;
+  // Use use state to notify react that this variable will change over time and must be updated
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const handleClick = (item: string) => console.log(item);
 
   const items3 = items.map((item, index) => (
     <li
@@ -9,7 +15,7 @@ function ListGroup() {
         index === selectedIndex ? "list-group-item active" : "list-group-item"
       }
       key={item}
-      onClick={() => (selectedIndex = index)}
+      onClick={() => setSelectedIndex(index)}
     >
       {item}
     </li>
@@ -17,7 +23,7 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List:</h1>
+      <h1 onClick={() => setSelectedIndex(-1)}>List:</h1>
       {items3.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">{items3.length !== 0 && items3}</ul>
     </>
