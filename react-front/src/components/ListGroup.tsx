aabaab27667796976b5e4693e897e2986ae38ef1
JawadBakhtiari <sixrip7er@gmail.com>
2023-12-05 18:01:07 +1100

    Highlight List, without using state

--- react-front/src/components/ListGroup.tsx
+++ react-front/src/components/ListGroup.tsx
@@ -1,17 +1,25 @@
-// function printList(items: any) {
-//     if(items.length ==)
-// }
-
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "Paris"];
   //   items = [];
-  const items3 = items.map((item) => <li key={item}>{item}</li>);
+  let selectedIndex = -1;
+
+  const items3 = items.map((item, index) => (
+    <li
+      className={
+        index === selectedIndex ? "list-group-item active" : "list-group-item"
+      }
+      key={item}
+      onClick={() => (selectedIndex = index)}
+    >
+      {item}
+    </li>
+  ));
 
   return (
     <>
       <h1>List:</h1>
       {items3.length === 0 && <p>No Item Found</p>}
-      {items3.length !== 0 && items3}
+      <ul className="list-group">{items3.length !== 0 && items3}</ul>
     </>
   );
 }
