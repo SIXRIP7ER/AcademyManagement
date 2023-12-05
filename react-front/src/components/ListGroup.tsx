cefd50f202a97fa970640e2d185fb5fc4b0571ba
JawadBakhtiari <sixrip7er@gmail.com>
2023-12-05 14:20:16 +1100

    1. Conditional Rendering If, Else method

--- react-front/src/components/ListGroup.tsx
+++ react-front/src/components/ListGroup.tsx
@@ -1,12 +1,21 @@
+function returnList(items3: any) {
+  if (items3.length === 0) {
+    return <p>No Item Found</p>;
+  } else {
+    return <ul className="list-group">{items3}</ul>;
+  }
+}
+
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "Paris"];
+  //   items = [];
+  const items3 = items.map((item) => <li key={item}>{item}</li>);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List:</h1>
+      {returnList(items3)}
+    </>
   );
 }
 
